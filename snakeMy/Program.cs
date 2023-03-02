2c785dd6c327e8e357354f17e7b2b4fe05528162
Aleksandr <bas8319@yahoo.com>
2023-03-02 22:23:01 +0300

    class Main

---
+++ snakeMy/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Snake
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            HorizontalLine hLine1 = new HorizontalLine(5, 20, 8, '+');
+            VerticalLine vLine = new VerticalLine(20, 8, 20, '+');
+            Point p1 = new Point(1, 5, '*');
+            p1.Draw();
+            hLine1.DrawLine();
+            vLine.DrawLine();
+
+
+
+        }
+
+    }
+}
+

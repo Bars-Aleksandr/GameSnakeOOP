7608b0fcbc2c35da59d651e64d48b567e5d4eadb
Aleksandr <bas8319@yahoo.com>
2023-03-02 22:45:14 +0300

    class Point

---
+++ snakeMy/Point.cs
@@ -0,0 +1,24 @@
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+        public Point()
+        {
+
+        }
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            System.Console.Write(sym);
+        }
+    }
+}
